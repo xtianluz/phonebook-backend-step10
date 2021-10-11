b9587c2e5c570fa6529406ec7cd8ae17a5b27501
Christian <xtianluz@gmail.com>
2021-10-12 01:21:20 +0800

    phonebook full stack

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(express.json())
+app.use(express.static('build'))
 
 const morgan = require('morgan')
 const morganFormat = ":method :url :status :res[content-length] - :response-time ms"
